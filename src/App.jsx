2ac34761e50302254524bb028b3b4d54acaefe2b
Ariel Duran <airmig@gmail.com>
2025-01-24 10:54:59 -0600

    added default base url

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (<>
     <div className='container-fluid'>
-    <Router>
+    <Router basename="/portfolio">
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
